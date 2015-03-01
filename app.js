124f23ae2e41fef6d1b4696528bdc8344238f797
karenchu <karen.alison.chu@gmail.com>
2015-03-01 02:00:55 -0800

    begun implementation of gun

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express'),
-  bodyParser = require('body-parser'),
-  ejs = require('ejs'),
-  app = express();
+  	bodyParser = require('body-parser'),
+  	ejs = require('ejs'),
+  	Gun = require('gun'),
+  	gun = Gun({file:"data.json"}),
+  	app = express();
 
+gun.attach(app);
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -10,7 +13,7 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
   // use a render
-  res.render('index.ejs');
+  res.render('home.ejs');
 });
 
 app.listen(process.env.PORT || 3000, function() {
